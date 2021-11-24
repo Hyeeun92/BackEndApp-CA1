d7f6a38570a74118b7ec15219965f9bb07aa9633
Hyeeun92 <21545@student.dorset-cllege.ie>
2021-11-24 00:02:43 +0000

    24.10

--- server.js
+++ server.js
@@ -1,105 +1,138 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const ejs = require("ejs")
-const { MongoDriverError } = require("mongodb")
-const port = 5000
-
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-
+const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:admin@gymmemberapi.6ztao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true })
+const db = mongoose.connection;
 
-const memberSchema = {
+db.once('error', () => {
+    console.log(err);
+});
+db.once('open', () => {
+    console.log("DB connnected")
+});
+
+const memberSchema = mongoose.Schema({
     name: String,
     gender: String,
     yearOfBirth: Number,
-    memberId: String,
     personalTraining: Boolean,
     facility: {
         locker: Boolean,
         poor: Boolean,
         shower: Boolean
     }
-}
+})
+const Member = mongoose.model('member', memberSchema)
 
-const Member = mongoose.model("members", memberSchema)
+const express = require("express")
+const app = express()
+
+const bodyParser = require("body-parser");
+const { render } = require('ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+const port = 5000
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
-
-app.get("/list", (req, res) => {
-    Member.find({}, (err, members) => {
+    Member.find({}, (err, member) => {
+        if (err) return res.json(err)
         res.render('index', {
-            memberList: members
+            member: member
         })
     })
 })
 
-app.post("/", (req, res) => {
-    let getBirth = req.body.yearOfBirth
-    let stringBirth = getBirth.toString()
-    let setId = req.body.name + stringBirth
-
-    let newMember = new Member({
-        name: req.body.name,
-        gender: req.body.gender,
-        yearOfBirth: req.body.yearOfBirth,
-        memberId: setId,
-        personalTraining: req.body.personalTraining,
-        facility: {
-            locker: req.body.locker,
-            poor: req.body.poor,
-            shower: req.body.shower
-        }
-
-    })
-    if (newMember.name != null && newMember.name != 'undefine' && newMember.yearOfBirth != null && newMember.yearOfBirth != 'undefine') {
-        newMember.save()
-        res.redirect('/')
-    } else console.log("missing")
+app.get('/create', (req, res) => {
+    res.render('create')
 })
 
-app.delete('/list', (req, res) => {
-    Member.deleteOne(
-        { memberId: req.body.id }
-    ).then(result => {
-        if (result.deletedCount === 0) {
-            return res.json('cannot delete')
-        }
-        res.json('Deleted')
+
+app.post('/create', (req, res) => {
+    Member.create(req.body, (err, member) => {
+        if (err) return res.json(err);
+        res.redirect('/');
     })
-        .catch(error => console.error(error))
-        console.log("id is here" + req.body.id)
 })
 
-app.put('/list', (req, res) => {
-    Member.findOneAndUpdate(
-        { memberId: req.body.id },
+app.get('/:id', (req, res) => {
+    Member.findOne({ _id: req.params.id }, (err, member) => {
+        if(err) return res.json(err);
+        res.render('read', { member: member });
+    })
+})
+
+app.get('/delete/:id', (req, res) => {
+    Member.deleteOne({ _id: req.params.id }, (err, member) => {
+        if (err) return res.json(err);
+        res.redirect('/');
+    });
+});
+
+app.get('/update/:id', (req, res) => {
+    Member.findOne({ _id: req.params.id }, (err, member) => {
+        if (err) return res.json(err);
+        res.render('update', { member: member });
+    });
+});
+
+app.post('/update/:id', (req, res) => {
+    Member.updateOne(
+        { _id: req.params.id },
         {
-            name: req.body.name,
-            gender: req.body.gender,
-            yearOfBirth: req.body.yearOfBirth,
-            memberId: req.body.id,
-            personalTraining: req.body.personalTraining,
-            facility: {
-                locker: req.body.locker,
-                poor: req.body.poor,
-                shower: req.body.shower
+            $set: {
+                name: req.body.name,
+                gender: req.body.gender,
+                yearOfBirth: req.body.yearOfBirth,
+                personalTraining: req.body.personalTraining,
+                facility: {
+                    locker: req.body.locker,
+                    poor: req.body.poor,
+                    shower: req.body.shower
+                }
             }
         },
-        { upsert: true }
-    ).then(result => res.json('Success'))
-        .catch(error => console.error(error))
-console.log("id is here" + req.body.id)
-})
+        (err, member) => {
+            if (err) return res.json(err);
+            res.redirect('/');
+        });
+});
 
 
-app.listen(port, () => {
-    console.log("server running")
-})
+    // app.delete('/list', (req, res) => {
+    //     Member.deleteOne(
+    //         { memberId: req.body.id }
+    //     ).then(result => {
+    //         if (result.deletedCount === 0) {
+    //             return res.json('cannot delete')
+    //         }
+    //         res.json('Deleted')
+    //     })
+    //         .catch(error => console.error(error))
+    //     console.log("id is here" + req.body.id)
+    // })
+
+    // app.put('/list', (req, res) => {
+    //     Member.findOneAndUpdate(
+    //         { memberId: req.body.id },
+    //         {
+    //             name: req.body.name,
+    //             gender: req.body.gender,
+    //             yearOfBirth: req.body.yearOfBirth,
+    //             memberId: req.body.id,
+    //             personalTraining: req.body.personalTraining,
+    //             facility: {
+    //                 locker: req.body.locker,
+    //                 poor: req.body.poor,
+    //                 shower: req.body.shower
+    //             }
+    //         },
+    //         { upsert: true }
+    //     ).then(result => res.json('Success'))
+    //         .catch(error => console.error(error))
+    //     console.log("id is here" + req.body.id)
+    // })
+
+
+    app.listen(port, () => {
+        console.log("server running")
+    })
