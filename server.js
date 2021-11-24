b128e8552407a0ee619b0e8623e09d105b5d9d6c
Hyeeun92 <21545@student.dorset-cllege.ie>
2021-11-24 11:09:03 +0000

    add comment

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin@gymmemberapi.6ztao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true })
-const db = mongoose.connection;
 
+//decraer and import express to connect firebase server
+const mongoose = require('mongoose')
+const db = mongoose.connection;
 db.once('error', () => {
     console.log(err);
 });
 db.once('open', () => {
     console.log("DB connnected")
 });
+const express = require("express")
+const app = express()
+const path = require('path')
+//make the app use the bodyParser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+const port = 5000
+//for style.css to call static 
+app.use(express.static(path.join(__dirname, 'public')))
 
+//set the gymmember schema
 const memberSchema = mongoose.Schema({
     name: String,
     gender: String,
@@ -22,19 +34,8 @@ const memberSchema = mongoose.Schema({
 })
 const Member = mongoose.model('member', memberSchema)
 
-const express = require("express")
-const app = express()
-const path = require('path')
-
-const bodyParser = require("body-parser");
-const { render } = require('ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-const port = 5000
-
-app.use(express.static(path.join(__dirname, 'public')))
-
+//get list from firebase to list in :localhost:port
+//call index.ejs
 app.get("/", (req, res) => {
     Member.find({}, (err, member) => {
         if (err) return res.json(err)
@@ -44,11 +45,12 @@ app.get("/", (req, res) => {
     })
 })
 
+//call create.ejs in: localhost:port/create
 app.get('/create', (req, res) => {
     res.render('create')
 })
 
-
+//send data to firebase in :localhost:port/create
 app.post('/create', (req, res) => {
     Member.create(req.body, (err, member) => {
         if (err) return res.json(err);
@@ -56,6 +58,7 @@ app.post('/create', (req, res) => {
     })
 })
 
+//find data from firebase with unique id in: localhost:port/unique id
 app.get('/:id', (req, res) => {
     Member.findOne({ _id: req.params.id }, (err, member) => {
         if (err) return res.json(err);
@@ -63,6 +66,7 @@ app.get('/:id', (req, res) => {
     })
 })
 
+//delete data from firebase with unique id: localhost:port/delete/unique id
 app.get('/delete/:id', (req, res) => {
     Member.deleteOne({ _id: req.params.id }, (err, member) => {
         if (err) return res.json(err);
@@ -70,6 +74,7 @@ app.get('/delete/:id', (req, res) => {
     });
 });
 
+//call update.ejs in :localhost:port/update/unique id
 app.get('/update/:id', (req, res) => {
     Member.findOne({ _id: req.params.id }, (err, member) => {
         if (err) return res.json(err);
@@ -77,6 +82,7 @@ app.get('/update/:id', (req, res) => {
     });
 });
 
+//save data to firebase with unique id: localhost:port/update/unique id
 app.post('/update/:id', (req, res) => {
     Member.updateOne(
         { _id: req.params.id },
@@ -95,11 +101,12 @@ app.post('/update/:id', (req, res) => {
         },
         (err, member) => {
             if (err) return res.json(err);
+            //when it is done call index.ejs in : localhost:port/
             res.redirect('/');
         });
 });
 
-
+//start server
 app.listen(port, () => {
-    console.log("server running")
+    mongoose.connect('mongodb+srv://admin:admin@gymmemberapi.6ztao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true })
 })
